dded19f2c371400bf121419879a041f27446c52d
Davide Cangiano <cangiano.0497@gmail.com>
2023-09-11 14:26:43 +0200

    Exercise done (with bonus)

---
+++ src/main/java/org/exercise/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package org.exercise.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String greetings(){
+        return "homepage";
+    }
+
+    @GetMapping("/about")
+    public String aboutUs(){
+    return "about";
+    }
+    @GetMapping("/terms")
+    public String termsAndConditions(){
+    return "terms";
+    }
+
+}
